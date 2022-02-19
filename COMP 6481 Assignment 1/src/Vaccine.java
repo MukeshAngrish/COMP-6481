e7e39c57821ae32d55f9e4e1e68768e2ce6597c7
Mukesh K. Angrish <mukeshangrish@live.com>
2022-02-19 16:56:34 -0500

    Initial Commit for COMP 6481 problems

---
+++ COMP 6481 Assignment 1/src/Vaccine.java
@@ -0,0 +1,36 @@
+// ____________________________
+// Assignment 1
+// © Mukesh Kumar Angrish
+// Written by: Mukesh Kumar Angrish (Student ID - 40203596)
+// ____________________________
+
+/*
+ * #TODO: Explain what the program does
+ * */
+
+public class Vaccine {
+	private enum brand {};
+	private double dose;
+	private String expiry_date;
+	private long id;
+	private double price;
+	
+	public double getDose() {
+		return dose;
+	}
+	
+	public String getExpiryDate() {
+		return expiry_date;
+	}
+	
+	public long getID() {
+		return id;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	
+	
+}
